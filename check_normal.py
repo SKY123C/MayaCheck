887bb95d036d955ec07bb5a207a08279d767f06d
SKY123C <56999224+SKY123C@users.noreply.github.com>
2021-03-04 12:54:15 +0800

    Update check_normal.py

--- check_normal.py
+++ check_normal.py
@@ -1,34 +1,45 @@
 import maya.cmds as mc
 import maya.OpenMaya as OpenMaya
 import pymel.core as pm
+import logging
+all_objects = mc.ls(dag=1, type="transform", v=1)
 
 
-def check_normal(model):    # model: mesh,polygon
+def check_normal(objects):
 
-    if model.lower() == "polygon":
-        obj = mc.ls(sl=1)
-    else:
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    logger = logging.getLogger("mylog")
+    logger.setLevel(logging.ERROR)
+    handle = logging.FileHandler('D:/mayaCheckNormal.log')
+    formatter = logging.Formatter(LOG_FORMAT)
+    handle.setFormatter(formatter)
+    logger.addHandler(handle)
+    if not objects:
+        logger.error("{}:The scene lacks objects".format(mc.file(exn=1)))
         return
 
-    dag = pm.PyNode(obj[0]).__apimdagpath__()
-    it = OpenMaya.MItMeshPolygon(dag)
-    temp = OpenMaya.MFnMesh(dag)
-    ray_source = OpenMaya.MFloatPoint()
-    hit_points = OpenMaya.MFloatPointArray()
-    while not it.isDone():
-        center_point = it.center(OpenMaya.MSpace.kWorld)
+    for obj in objects:
+        dag = pm.PyNode(obj).__apimdagpath__()
+        it = OpenMaya.MItMeshPolygon(dag)
+        temp = OpenMaya.MFnMesh(dag)
+        ray_source = OpenMaya.MFloatPoint()
+        hit_points = OpenMaya.MFloatPointArray()
+        while not it.isDone():
+            center_point = it.center(OpenMaya.MSpace.kWorld)
 
-        face_normal = OpenMaya.MVector()
-        it.getNormal(face_normal, OpenMaya.MSpace.kWorld)
-        d_normal = face_normal*0.1
-        temp_point = center_point + d_normal
-        ray_source.setCast(temp_point)
-        b_intersection = temp.allIntersections(ray_source, OpenMaya.MFloatVector(face_normal),
-                                              None, None, False, OpenMaya.MSpace.kWorld, 999, False, None, False,
-                                              hit_points, None, None, None, None, None)
-        if b_intersection and hit_points.length() % 2 != 0:
-            print it.index()
+            face_normal = OpenMaya.MVector()
+            it.getNormal(face_normal, OpenMaya.MSpace.kWorld)
+            d_normal = face_normal * 0.1
+            temp_point = center_point + d_normal
+            ray_source.setCast(temp_point)
+            b_intersection = temp.allIntersections(ray_source, OpenMaya.MFloatVector(face_normal),
+                                                   None, None, False, OpenMaya.MSpace.kWorld, 999, False, None, False,
+                                                   hit_points, None, None, None, None, None)
+            if b_intersection and hit_points.length() % 2 != 0:
+                logger.warning("%s.f[%d]" % (obj, it.index()))
 
-        it.next()
+            it.next()
 
-check_normal("polygon")
+
+check_normal(all_objects)
+
